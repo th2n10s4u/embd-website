24b6a7983b835b409d036c46a2eb2a7e521e6c80
th2n10s4u <th2n10s4u@gmail.com>
2022-07-01 20:22:19 -0700

    Create TemplateTextCard.js
    Started creating a prop for text only items.
    TODO creat a bullet variable because mui doesn't have <ul>

---
+++ src/assets/components/TemplateTextCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { paper, box, card, CardContent, typography } from "@mui/material";
+
+export default function TemplateTextCard() {
+  return (
+    <Paper>
+      <Card>
+        <CardContent></CardContent>
+      </Card>
+    </Paper>
+  );
+}
