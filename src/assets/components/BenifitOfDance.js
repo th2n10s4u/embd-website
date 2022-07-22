14c889f1ac5df61b4a63b3860b59a4128c453225
th2n10s4u <th2n10s4u@gmail.com>
2022-07-22 05:32:14 -0700

    Create BenifitOfDance.js
    Created BenefitsOfDance

---
+++ src/assets/components/BenifitOfDance.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Paper, Box, Typography, ListItem, } from "@mui/material";
+import TemplateCard from "./TemplateCard";
+import TemplateTextCard from "./TemplateTextCard";
+import Fruit from "src/assets/images/Fruit-dance-image.png";
+import FunDip from "src/assets/images/Fun-dip-imagwe.png";
+import Crossbody from "src/assets/images/Couple-crossbody-image.png"
+
+export default function Benefits() {
+    return (
+
+        
+
+    )
+}
