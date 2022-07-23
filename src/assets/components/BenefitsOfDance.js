0e11c32ae378f869c02a5aa615719a34407dcec8
th2n10s4u <th2n10s4u@gmail.com>
2022-07-22 22:05:37 -0700

    Update BenefitsOfDance.js
    working on  combining templatecard and templatetextcard in the same component.

--- src/assets/components/BenefitsOfDance.js
+++ src/assets/components/BenefitsOfDance.js
@@ -2,22 +2,64 @@ import React from "react";
 import { Paper, Box, Typography, ListItem } from "@mui/material";
 import TemplateCard from "./TemplateCard";
 // import TemplateTextCard from "./TemplateTextCard";
-import Fruit from "../images/Fruit-dance-image.png";
-// import FunDip from "src/assets/images/Fun-dip-imagwe.png";
+// import Fruit from "../images/Fruit-dance-image.png";
+import FunDip from "../images/Fun-dip-image.png";
+import TemplateTextCard from "./TemplateTextCard";
 // import Crossbody from "src/assets/images/Couple-crossbody-image.png";
 
 export default function BenefitsOfDance() {
   return (
-    <Box sx={{ display: "flex", justifyContent: "center" }}>
-      <TemplateCard
-        img={Fruit}
-        CardContentHeaderFirstLine="Why Dance?"
-        CardContentBodySentenceOne="The number of people taking part in dance classes is increasing worldwide, helping people to become more social within their local communities, improve their physical fitness and develop creative skills."
-        CardContentBodySentenceTwo=" There are many benefits to partaking in dance classes ranging from increasing your amount of daily exercise to making new friends with shared interests. Why is it that many dancers enjoy dance for a lifetime? See some of the benefits that people of all ages enjoy below."
-        centerHeader
-        centerBodyOne
-        centerBodyTwo
-      />
-    </Box>
+    <React.Fragment>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
+        }}
+      >
+        <TemplateCard
+          img={FunDip}
+          CardContentHeaderFirstLine="Why Dance?"
+          CardContentBodySentenceOne="The number of people taking part in dance classes is increasing worldwide, helping people to become more social within their local communities, improve their physical fitness and develop creative skills."
+          CardContentBodySentenceTwo=" There are many benefits to partaking in dance classes ranging from increasing your amount of daily exercise to making new friends with shared interests. Why is it that many dancers enjoy dance for a lifetime? See some of the benefits that people of all ages enjoy below."
+          centerHeader
+          centerBodyOne
+          centerBodyTwo
+          reverse
+          background
+          shadow
+          radius
+        />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "",
+            justifyContent: "space-between",
+          }}
+        >
+          <TemplateCard img={FunDip} />
+          {/* <TemplateTextCard
+            TextCardHeader="Benefits?"
+            ListText={[
+              "Healthy Posture",
+              "Better Balance",
+              "Increased Endurance",
+              "Social Satisfaction",
+              "Reduces Stress",
+              "New Neural Pathway Creation",
+              "Diminished Depression",
+              "Boost Memory",
+              "Improved Flexibility",
+              "Enhanced Communication Skills",
+              "Cardiovascular Health",
+              "Creative Outlet",
+            ]}
+            
+            /> */}
+        </Box>
+      </Box>
+    </React.Fragment>
   );
+}
+{
 }
