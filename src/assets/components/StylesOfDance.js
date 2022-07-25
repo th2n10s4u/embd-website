efd619170546c894cc164542cbbe51f8557f138f
th2n10s4u <th2n10s4u@gmail.com>
2022-07-25 06:57:54 -0700

    Update StylesOfDance.js
    fixed width to center container.

--- src/assets/components/StylesOfDance.js
+++ src/assets/components/StylesOfDance.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, Container, Paper, Typography } from "@mui/material";
 import LatinDip from "../images/latin-dip-image.png";
 import TemplateCard from "./TemplateCard";
 import TemplateTextCard from "./TemplateTextCard";
@@ -7,83 +7,110 @@ import TemplateTextCard from "./TemplateTextCard";
 export default function StylesOfDance() {
   return (
     <React.Fragment>
-      <Box sx={{ display: "flex", justifyContent: "space-around" }}>
-        <TemplateCard
-          img={LatinDip}
-          CardContentBodySentenceOne="Evelyn Meinz Ballroom Dance teaches all styles of partnership dancing. From sophisticated to spicy we’ve got you covered!"
-          reverse
-          background
-          shadow
-          radius
-          centerBodyOne
-        />
-      </Box>
-
-      <Box
+      <Paper
+        elevation={0}
         sx={{
           display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          maxWidth: "900px",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "Center",
         }}
       >
+        <Box sx={{ display: "flex", justifyContent: "space-around" }}>
+          <TemplateCard
+            img={LatinDip}
+            CardContentBodySentenceOne="Evelyn Meinz Ballroom Dance teaches all styles of partnership dancing. From sophisticated to spicy we’ve got you covered!"
+            reverse
+            background
+            shadow
+            radius
+            centerBodyOne
+          />
+        </Box>
+
+        <Container
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            // maxWidth:"900px",
+            // minWidth:"813px",
+            width: "900px",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              maxWidth: "900px",
+            }}
+          >
+            <TemplateTextCard
+              TextCardHeader="Social Styles"
+              ListText={[
+                "Salsa",
+                "Tango",
+                "Cha Cha",
+                "Bachata",
+                "Hustle",
+                "Swing",
+                "Country Western",
+                "Merengue",
+              ]}
+            />
+
+            <TemplateTextCard
+              TextCardHeader="International"
+              ListText={[
+                "Foxtrot",
+                "Quickstep",
+                "Viennese Waltz",
+                "Slow Waltz",
+                "Tango",
+              ]}
+            />
+
+            <TemplateTextCard
+              TextCardHeader="Latin"
+              ListText={["Rumba", "Cha Cha", "Jive", "Paso Doble", "Samba"]}
+            />
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              maxWidth: "900px",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+            }}
+          >
+            <TemplateTextCard
+              TextCardHeader="Smooth"
+              ListText={[
+                "Foxtrot",
+                "Waltz",
+                "Viennese Waltz",
+                "Peabody",
+                "Tango",
+              ]}
+            />
+
+            <TemplateTextCard
+              TextCardHeader="Rythm"
+              ListText={["Rumba", "Cha Cha", "Swing", "Bolero", "Mambo"]}
+            />
+          </Box>
+        </Container>
         <TemplateTextCard
-          TextCardHeader="Social Styles"
           ListText={[
-            "Salsa",
-            "Tango",
-            "Cha Cha",
-            "Bachata",
-            "Hustle",
-            "Swing",
-            "Country Western",
-            "Merengue",
+            "Not sure what dance style is right for you?",
+            "Don't worry! Just Tell us your goals and we can help you pick the right dances to learn",
           ]}
         />
-
-        <TemplateTextCard
-          TextCardHeader="International"
-          ListText={[
-            "Foxtrot",
-            "Quickstep",
-            "Viennese Waltz",
-            "Slow Waltz",
-            "Tango",
-          ]}
-        />
-
-        <TemplateTextCard
-          TextCardHeader="Latin"
-          ListText={["Rumba", "Cha Cha", "Jive", "Paso Doble", "Samba"]}
-        />
-      </Box>
-
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          maxWidth: "900px",
-          justifyContent: "space-evenly",
-          alignItems: "center",
-        }}
-      >
-        <TemplateTextCard
-          TextCardHeader="Smooth"
-          ListText={["Foxtrot", "Waltz", "Viennese Waltz", "Peabody", "Tango"]}
-        />
-
-        <TemplateTextCard
-          TextCardHeader="Rythm"
-          ListText={["Rumba", "Cha Cha", "Swing", "Bolero", "Mambo"]}
-        />
-      </Box>
-
-      <TemplateTextCard
-        ListText={[
-          "Not sure what dance style is right for you?",
-          "Don't worry! Just Tell us your goals and we can help you pick the right dances to learn",
-        ]}
-      />
+      </Paper>
     </React.Fragment>
   );
 }
