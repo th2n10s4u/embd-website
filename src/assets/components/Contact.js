4153cb01799e2150c3ebe9de9c1a6ea706df5372
th2n10s4u <th2n10s4u@gmail.com>
2022-07-25 06:23:59 -0700

    Update Contact.js
    Added containers
    adjusted alignment
    todo: alignment / containers.

--- src/assets/components/Contact.js
+++ src/assets/components/Contact.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Card, Box, CardContent, Paper, Typography } from "@mui/material";
+import {
+  Card,
+  Box,
+  CardContent,
+  Container,
+  Paper,
+  Typography,
+} from "@mui/material";
 import ContactForm from "../components/ContactForm";
 
 export default function Contact() {
@@ -9,105 +16,128 @@ export default function Contact() {
         elevation={0}
         sx={{
           minHeight: 700,
+          maxWidth: 900,
           display: "flex",
           flexDirection: "row",
-          justifyContent: "space-evenly",
-          aligncontent: "space-evenly",
+          justifyContent: "center",
+          alignContent: "center",
         }}
       >
-        <Card
-          elevation={0}
+        <Container
           sx={{
-            maxWidth: "350px",
-            maxHeight: 550,
-            alignSelf: "center",
-            marginBottom: "25px",
-            background: "#DEE2E9",
-            boxShadow: "9px 9px 6px grey",
-            borderRadius: "12px",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-end",
+            alignContent: "center",
+            width: "650px",
+            Height: "550px",
           }}
         >
-          <CardContent>
-            <Typography
-              sx={{
-                fontFamily: "Playfair Display",
-                fontWeight: "600",
-                fontSize: "28px",
-                lineHeight: "30px",
-                textAlign: "center",
-              }}
-            >
-              We look forward to hearing from you soon!
-            </Typography>
-            <Typography
-              sx={{
-                fontFamily: "Playfair Display",
-                fontWeight: "400",
-                fontSize: "24px",
-                lineHeight: "40px",
-                textAlign: "center",
-              }}
-            >
-              Phone: 123456689
-            </Typography>
-            <Typography
-              sx={{
-                fontFamily: "Playfair Display",
-                fontWeight: "400",
-                fontSize: "24px",
-                lineHeight: "40px",
-                textAlign: "center",
-              }}
-            >
-              Email embd@embd.com
-            </Typography>
-            <ContactForm />
-          </CardContent>
-        </Card>
-        <Card
-          elevation={0}
+          <Card
+            elevation={0}
+            sx={{
+              maxWidth: "350px",
+              maxHeight: 550,
+              alignSelf: "center",
+              marginBottom: "25px",
+              background: "#DEE2E9",
+              boxShadow: "9px 9px 6px grey",
+              borderRadius: "12px",
+            }}
+          >
+            <CardContent>
+              <Typography
+                sx={{
+                  fontFamily: "Playfair Display",
+                  fontWeight: "600",
+                  fontSize: "28px",
+                  lineHeight: "30px",
+                  textAlign: "center",
+                }}
+              >
+                We look forward to hearing from you soon!
+              </Typography>
+              <Typography
+                sx={{
+                  fontFamily: "Playfair Display",
+                  fontWeight: "400",
+                  fontSize: "24px",
+                  lineHeight: "40px",
+                  textAlign: "center",
+                }}
+              >
+                Phone: 123456689
+              </Typography>
+              <Typography
+                sx={{
+                  fontFamily: "Playfair Display",
+                  fontWeight: "400",
+                  fontSize: "24px",
+                  lineHeight: "40px",
+                  textAlign: "center",
+                }}
+              >
+                Email embd@embd.com
+              </Typography>
+              <ContactForm />
+            </CardContent>
+          </Card>
+        </Container>
+        <Container
           sx={{
-            maxWidth: 650,
-            maxHeight: 550,
-            alignSelf: "center",
-            marginBottom: "25px",
-            background: "#DEE2E9",
-            boxShadow: "9px 9px 6px grey",
-            borderRadius: "12px",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            alignContent: "center",
+            width: "650px",
+            Height: "550px",
           }}
         >
-          <CardContent>
-            <Typography
-              sx={{
-                fontFamily: "Playfair Display",
-                fontWeight: "400",
-                fontSize: "28px",
-                lineHeight: "30px",
-                textAlign: "center",
-              }}
-            >
-              Address:
-            </Typography>
-            <Typography
-              sx={{
-                fontFamily: "Playfair Display",
-                fontWeight: "400",
-                fontSize: "28px",
-                lineHeight: "20px",
-                textAlign: "center",
-              }}
-            >
-              123 street, city, 95404
-            </Typography>
-            <Box
-              ClassName="MapBox"
-              sx={{
-                height: "400px",
-                width: "500px",
-              }}
-            ></Box>
-          </CardContent>
-        </Card>
+          <Card
+            elevation={0}
+            sx={{
+              maxWidth: 650,
+              maxHeight: 550,
+              alignSelf: "center",
+              marginBottom: "25px",
+              background: "#DEE2E9",
+              boxShadow: "9px 9px 6px grey",
+              borderRadius: "12px",
+            }}
+          >
+            <CardContent>
+              <Typography
+                sx={{
+                  fontFamily: "Playfair Display",
+                  fontWeight: "400",
+                  fontSize: "28px",
+                  lineHeight: "30px",
+                  textAlign: "center",
+                }}
+              >
+                Address:
+              </Typography>
+              <Typography
+                sx={{
+                  fontFamily: "Playfair Display",
+                  fontWeight: "400",
+                  fontSize: "28px",
+                  lineHeight: "20px",
+                  textAlign: "center",
+                }}
+              >
+                123 street, city, 95404
+              </Typography>
+              <Box
+                ClassName="MapBox"
+                sx={{
+                  height: "400px",
+                  width: "500px",
+                }}
+              ></Box>
+            </CardContent>
+          </Card>
+        </Container>
       </Paper>
     </React.Fragment>
   );
