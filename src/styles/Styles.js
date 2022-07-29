f0a368549b988aface9325e27c45509b074d7ce3
th2n10s4u <th2n10s4u@gmail.com>
2022-07-29 06:24:07 -0700

    Create Styles.js
    Created theme

---
+++ src/styles/Styles.js
@@ -0,0 +1,40 @@
+import { createTheme } from "@mui/material/styles";
+
+const Colors = {
+  primary: "#DEE2E9",
+  secondary: "#C34AC2",
+  // success:
+  info: "#000",
+  // danger:
+  // warning:
+  // dark:
+  light: "#aaa",
+  // muted:
+  // border:
+  // inverse:
+  //////////////
+  //Solid Color
+  //////////////
+  white: "#fff",
+  black: "#000",
+};
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: Colors.primary,
+    },
+    secondary: {
+      main: Colors.secondary,
+    },
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+  },
+});
+
+export default theme;
