c67dc0dff8fae3fdd288d661ca543b5b4a5a3d0d
th2n10s4u <th2n10s4u@gmail.com>
2022-08-05 10:46:17 -0700

    Create ReadMore.js
    Created component for truncation

---
+++ src/assets/components/ReadMore.js
@@ -0,0 +1,55 @@
+import { React, useState } from "react";
+import Truncate from "react-truncate";
+import { Typography } from "@mui/material";
+
+export default function ReadMore(props) {
+  const [expanded, setExpanded] = useState(false);
+  const [trunk, setTrunk] = useState(false);
+  // const longText = ["array of text 1","array of text 1","array of text 1"];
+  const toggleLines = (e) => {
+    e.preventDefault();
+    setExpanded((prev) => !prev);
+  };
+  const handleTruncate = (truncated) => {
+    setTrunk(truncated);
+  };
+
+  return (
+    <Typography
+      ClassName="CardHeaderText"
+      sx={{
+        fontFamily: "Poppins",
+        fontWeight: "400",
+        FontSize: "16px",
+        lineHeight: "24.43px",
+        letterSpacing: "0.05em",
+        letterSpacing: "5%",
+        textAlign: "center",
+        // marginTop: "20px", lowers the bottom margin of the card but allows the text to fit properly.
+      }}
+    >
+      <Truncate
+        lines={!expanded && 5}
+        ellipsis={
+          <span>
+            ...{" "}
+            <a href="#" onClick={toggleLines}>
+              Read more
+            </a>
+          </span>
+        }
+        onTruncate={handleTruncate}
+      >
+        {props.children}
+      </Truncate>
+      {!trunk && expanded && (
+        <span>
+          {" "}
+          <a href="#" onClick={toggleLines}>
+            Show less
+          </a>
+        </span>
+      )}
+    </Typography>
+  );
+}
