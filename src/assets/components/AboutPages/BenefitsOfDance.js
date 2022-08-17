5a6b56ec20685251b977dc64cb3f141f6fab26f3
th2n10s4u <th2n10s4u@gmail.com>
2022-08-17 10:31:48 -0700

    Update BenefitsOfDance.js
    Added reverse prop to "science" component.

--- src/assets/components/AboutPages/BenefitsOfDance.js
+++ src/assets/components/AboutPages/BenefitsOfDance.js
@@ -83,6 +83,7 @@ export default function BenefitsOfDance() {
             centerBodyOne
             centerBodyTwo
             centerBodyThree
+            reverse
           />
         </Box>
       </Box>
