d53f9eb9b35b8fcac7e5cc8c6dc61cc84450a18c
th2n10s4u <th2n10s4u@gmail.com>
2022-08-23 15:00:40 -0700

    Update ContactForm.js
    Added state, callback function, submit, actionform fetch.

--- src/assets/components/TemplateComponents/ContactForm.js
+++ src/assets/components/TemplateComponents/ContactForm.js
@@ -1,7 +1,53 @@
-import React, { Form } from "react";
+import React, { useState } from "react";
 import { Box, TextField, Button } from "@mui/material";
 
 export default function ContactForm() {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const handleInputChange = (event) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    alert(
+      "You're information has been sent and you will be contacted within 1-2 business days."
+    );
+    console.log(inputs);
+    ///
+    var formdata = new FormData();
+    formdata.append("name", inputs.name);
+    formdata.append("email", inputs.email);
+    formdata.append("phone", inputs.phone);
+
+    var requestOptions = {
+      method: "POST",
+      body: JSON.stringify(inputs),
+      redirect: "follow",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    };
+
+    fetch(
+      "https://www.actionforms.io/e/r/evelyn-meinz-ballroom-dance",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  };
+
+  ///
   return (
     <Box
       sx={{
@@ -10,33 +56,41 @@ export default function ContactForm() {
         maxWidth: "313px",
       }}
     >
-      <Form action="https://www.actionforms.io/e/r/evelyn-meinz-ballroom-dance">
+      <form
+        action="https://www.actionforms.io/e/r/evelyn-meinz-ballroom-dance"
+        onSubmit={handleSubmit}
+      >
         <TextField
+          value={inputs.name}
           label={"NAME"}
+          onChange={handleInputChange}
           id="FormName"
-          name="FormName"
+          name="name"
           margin="normal"
           variant="filled"
           autoComplete="none"
         />
         <TextField
+          value={inputs.email}
           label={"EMAIL"}
+          onChange={handleInputChange}
           id="FormEmail"
-          name="FormEmail"
+          name="email"
           margin="normal"
           variant="filled"
           autoComplete="none"
         />
         <TextField
+          value={inputs.phone}
           label={"PHONE"}
+          onChange={handleInputChange}
           id="FormPhone"
-          name="FormPhone"
+          name="phone"
           margin="normal"
           variant="filled"
           autoComplete="none"
         />
         <Button
-          type="submit"
           style={{
             minWidth: "313px",
             minHeight: "40px",
@@ -50,12 +104,11 @@ export default function ContactForm() {
           display="flex"
           boxShadow="9px 9px 6px grey"
           background="#fffff"
+          type="submit"
         >
           Submit
         </Button>
-      </Form>
+      </form>
     </Box>
   );
 }
-
-//TODO: dropshadow, give the container around the form and the header a fixed width, then text-align: centering the header. Other option is to give the container align-items: center, but then the box won’t be left aligned.
