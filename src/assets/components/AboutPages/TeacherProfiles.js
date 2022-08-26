550e6d8fe898bd8982d02dc99c7a2c1a663b429b
th2n10s4u <th2n10s4u@gmail.com>
2022-08-26 07:28:33 -0700

    Update TeacherProfiles.js
    alignment

--- src/assets/components/AboutPages/TeacherProfiles.js
+++ src/assets/components/AboutPages/TeacherProfiles.js
@@ -73,6 +73,7 @@ export default function TeacherProfiles() {
         sx={{
           display: "flex",
           justifyContent: "center",
+          alignItems: "center",
         }}
       >
         <TemplateCard
