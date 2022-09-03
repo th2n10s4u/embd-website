1e467face3d6dc7aea73364b8a1f684d56dd6263
th2n10s4u <th2n10s4u@gmail.com>
2022-09-02 23:17:51 -0700

    Added Swiperjs
    Removed React-material-ui-carousel and added Swiperjs

--- src/assets/components/MainComponents/Carousel.js
+++ src/assets/components/MainComponents/Carousel.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { Paper } from "@mui/material";
 import Carousel from "react-material-ui-carousel";
+///////
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, EffectFade } from "swiper";
+import "swiper/swiper.min.css";
+// import "swiper/css";
+// import "swiper/css/effect-fade";
 import BeachPhoto from "../../images/Carousel-Beach-Image.png";
 import ForestPhoto from "../../images/Carousel-Forest-Couple-Image.png";
 import GroupDance from "../../images/Carousel-Group-Dance.png";
@@ -8,39 +15,63 @@ import WeddingDance from "../../images/Carousel-Weeding-Image.png";
 //Remove initial render animation sizing issue
 //Possibly add a prop to animateOnRender: boolean ?
 
-export default function MainCarousel(props) {
-  var items = [
-    {
-      imgUrl: BeachPhoto,
-    },
-    {
-      imgUrl: ForestPhoto,
-    },
-    {
-      imgUrl: GroupDance,
-    },
-    {
-      imgUrl: WeddingDance,
-    },
-  ];
-
-  console.log(items);
+export default function MainCarousel() {
   return (
-    <Carousel>
-      {items.map((item, i) => (
-        <Item key={i} item={item} />
-      ))}
-    </Carousel>
-  );
-}
-{
-}
-function Item({ item }) {
-  return (
-    <Paper elevation={0} sx={{ height: "800px" }}>
-      <img src={item.imgUrl} alt="CarouselImages" width="100%"></img>
-    </Paper>
+    <Swiper
+      modules={[Navigation, EffectFade]}
+      effect
+      speed={800}
+      slidesPerView={1}
+      loop
+    >
+      <Paper sx={{ height: "1800px", width: "100%" }}>
+        <SwiperSlide>
+          <img src={BeachPhoto} alt="Beach-Photo" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={ForestPhoto} alt="Beach-Photo" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={GroupDance} alt="Beach-Photo" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={WeddingDance} alt="Beach-Photo" />
+        </SwiperSlide>
+      </Paper>
+    </Swiper>
   );
 }
 
-// TODO: Report but -On load carousel is collapsed.
+// export default function MainCarousel(props) {
+//   var items = [
+//     {
+//       imgUrl: BeachPhoto,
+//     },
+//     {
+//       imgUrl: ForestPhoto,
+//     },
+//     {
+//       imgUrl: GroupDance,
+//     },
+//     {
+//       imgUrl: WeddingDance,
+//     },
+//   ];
+
+//   return (
+//     <Carousel>
+//       {items.map((item, i) => (
+//         <Item key={i} item={item} />
+//       ))}
+//     </Carousel>
+//   );
+// }
+// {
+// }
+// function Item({ item }) {
+//   return (
+//     <Paper elevation={0} sx={{ height: "800px" }}>
+//       <img src={item.imgUrl} alt="CarouselImages" width="100%"></img>
+//     </Paper>
+//   );
+// }
